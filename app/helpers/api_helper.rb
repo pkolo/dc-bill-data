b294cd4e30633db07212eac3792f3b1db6337482
Patrick Kolodgy <pkolodgy@gmail.com>
2017-03-23 14:07:24 -0400

    Loop to get more bill data

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -4,12 +4,17 @@ require 'json'
 helpers do
 
   def todays_bills
-    res = HTTParty.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json?offset=0", headers: {"X-API-Key" => ENV['PROPUB_KEY']})
-    res["results"][0]["bills"].inject([]) do |memo, bill|
-      bill_data = HTTParty.get(bill["bill_uri"], headers: {"X-API-Key" => ENV['PROPUB_KEY']})
-      memo << bill_data["results"][0]
-      memo
+    memo = []
+    offset = 0
+    until offset == 60
+      res = HTTParty.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json?offset=#{offset}", headers: {"X-API-Key" => ENV['PROPUB_KEY']})
+      res["results"][0]["bills"].each do |bill|
+        bill_data = HTTParty.get(bill["bill_uri"], headers: {"X-API-Key" => ENV['PROPUB_KEY']})
+        memo << bill_data["results"][0]
+      end
+      offset += 20
     end
+    memo
   end
 
 end
