818cdd29d4c894c69c719d44da92d338998cffbb
Patrick Kolodgy <pkolodgy@gmail.com>
2017-03-23 10:03:29 -0400

    Getting bill details for each recent vill

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -5,7 +5,11 @@ helpers do
 
   def todays_bills
     res = HTTParty.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json?offset=0", headers: {"X-API-Key" => ENV['PROPUB_KEY']})
-    res["results"][0]["bills"]
+    res["results"][0]["bills"].inject([]) do |memo, bill|
+      bill_data = HTTParty.get(bill["bill_uri"], headers: {"X-API-Key" => ENV['PROPUB_KEY']})
+      memo << bill_data["results"][0]
+      memo
+    end
   end
 
 end
