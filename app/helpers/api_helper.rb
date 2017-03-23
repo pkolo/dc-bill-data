63a451708ffd170385ec368105ffef8ff825fb77
Patrick Kolodgy <pkolodgy@gmail.com>
2017-03-23 09:29:59 -0400

    Get response by bill

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -5,7 +5,7 @@ helpers do
 
   def todays_bills
     res = HTTParty.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json?offset=0", headers: {"X-API-Key" => ENV['PROPUB_KEY']})
-    res["results"]
+    res["results"][0]["bills"]
   end
 
 end
